988029b343edb255b306a14301c5b5e16ebddd05
natefoulk4 <natefoulk4@gmail.com>
2017-12-05 13:33:13 -0700

    math fixes

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+    args = {}
+except ImportError:
+    from distutils.core import setup
+    print("""\
+*** WARNING: setuptools is not found.  Using distutils...
+""")
+
+from setuptools import setup
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+from os import path
+setup(name='code',
+      version='0.0.1',
+      description='First unit testing for Ch2',
+      long_description= "" if not path.isfile("README.md") else read_md('README.md'),
+      author='Nate Foulk',
+      author_email='natefoulk4@gmail.com',
+      url='https://github.com/natefoulk4/Ch2_Giordano',
+      license='MIT',
+      setup_requires=['pytest-runner',],
+      tests_require=['pytest', 'python-coveralls'],
+      install_requires=[
+          "numpy",
+          "matplotlib",
+          "matplotlib.pyplot",
+      ],
+      packages=['code'],
+      # scripts=[''],
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 1 - planning',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Operating System :: Linux Ubuntu',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+      ],
+)
